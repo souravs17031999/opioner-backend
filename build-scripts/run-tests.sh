799154f9ccb4abebadfdf94593060307175d587e
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-16 19:26:25 +0530

    test

--- build-scripts/run-tests.sh
+++ build-scripts/run-tests.sh
@@ -2,5 +2,5 @@ echo "----------------- INNSIDE RUN TESTS FILE -------------------------"
 
 echo "----------------- Building Docker containers for apitest ..... "
 
-docker-compose -f test/docker-compose.yml up --build --abort-on-container-exit
+docker-compose -f test/docker-compose.yml up --build
 docker-compose -f test/docker-compose.yml down
