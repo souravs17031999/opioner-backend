ee33ca5a133a43c495348135b852eb3633933cc4
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-18 07:38:03 +0530

    TEST MOCHA WITH NEW HOST INTERSERVICE COMM. ENNABLED

--- auth-service-taskly/wait-for-db-server.sh
+++ auth-service-taskly/wait-for-db-server.sh
@@ -1,6 +1,6 @@
 echo "------------------------- WAITING FOR SERVER FOR POSTGRES DB -------------------------"
 
-TIMEOUT=10
+TIMEOUT=30
 
 # while [[ $TIMEOUT -gt 0 ]]; do 
 #     pg_isready -p 5432 
