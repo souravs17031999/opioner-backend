fdad80cfb6e08bd0bc921c9a2fd754e307eec340
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-21 22:10:21 +0530

    Add postgres db wait-for-server section for other services

--- test/wait-for-db-server.sh
+++ test/wait-for-db-server.sh
@@ -2,8 +2,8 @@ echo "------------------------- WAITING FOR AUTH-SERVICE SERVER: /auth  --------
 
 sleep 30
 
-servername=http://auth_service/auth/test
-response=$(curl --write-out '%{http_code}' --silent --output /dev/null $servername)
+$servername=http://auth_service/auth/test
+response=$(curl --write-out '%{http_code}' $servername)
 echo "------------- reponse: $response"
 
 echo "..... SERVER WAITING TIME FINISHED ...."
