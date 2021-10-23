750cef17b5e8db467604cf0fb20db2a62947169c
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-23 10:15:26 +0530

    Adding Test suite infrastructure

--- test/wait-for-db-server.sh
+++ test/wait-for-db-server.sh
@@ -1,9 +1,24 @@
-echo "------------------------- WAITING FOR AUTH-SERVICE SERVER: /auth  -------------------------"
+export TIMEOUT=120
+export AUTH_SERVER_URL=http://auth_service:8081/auth/test
 
-sleep 30
+echo "------------------------- WAITING FOR AUTH-SERVICE SERVER URL: $AUTH_SERVER_URL for $TIMEOUT seconds"
 
-export servername=http://auth_service:8081/auth/test
-response=$(curl --write-out '%{http_code}' $servername)
-echo "------------- reponse: $response"
+export status_code=200
 
-echo "..... SERVER WAITING TIME FINISHED ...."
+while [[ $TIMEOUT -gt 0 ]]; do 
+    status_code=$(curl -o /dev/null -s -w "%{http_code}\n" $AUTH_SERVER_URL)
+    if [[ $status_code -eq 200 ]]; then 
+        echo "AUTH SERVICE SERVER READY !!"
+        break
+    fi
+    echo "Waiting for Auth-service Server......"
+    sleep 1 
+    TIMEOUT=$(($TIMEOUT-1))
+done
+
+echo "..... AUTH-SERVICE SERVER WAITING TIME FINISHED ...."
+
+if [[ $status_code -ne 200 ]]; then 
+    echo "AUTH-SERVICE SERVER NOT READY !"
+    exit -1
+fi
