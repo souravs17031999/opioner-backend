d745c2de3d1a07ed0a8f15c794135bd2b6a1e816
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-24 11:34:00 +0530

    Revert previous PR for docker ports change

--- build-scripts/docker-flow.sh
+++ build-scripts/docker-flow.sh
@@ -1,8 +1,3 @@
 echo "Building docker image for taskly backend app ......"
 docker-compose -f docker-compose.yml build
-
-echo "Publishing to docker hub....."
-docker login -u $dockerHubUsername -p $dockerHubPassword
-export tagname=$BUILD_NUMBER
-docker push souravkumardevadmin/taskly-backend:$tagname
 docker-compose -f docker-compose.yml down
