ee6072822488cd06f7dddab43dec138118553273
souravs17031999 <souravs_1999@rediffmail.com>
2021-10-24 11:31:31 +0530

    Change docker build ports for testing on jenkins

--- build-scripts/publish.sh
+++
@@ -1,5 +0,0 @@
-echo "Publishing to docker hub....."
-docker login -u $dockerHubUsername -p $dockerHubPassword
-export tagname=$BUILD_NUMBER
-docker push souravkumardevadmin/taskly-backend:$tagname
-docker-compose -f docker-compose.yml down
