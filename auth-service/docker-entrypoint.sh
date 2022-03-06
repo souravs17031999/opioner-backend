48dfa63ddc01d7a1dab3643a74a9a721ba6a12b0
souravs17031999 <souravs_1999@rediffmail.com>
2022-03-06 19:03:15 +0530

    wip

--- auth-service/docker-entrypoint.sh
+++ auth-service/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 export PYTHONUNBUFFERED="true"
-source env.sh
+source /app/env.sh
 source wait-for-db-server.sh
 if [[ "$REQUIRE_DB_MIGRATIONS" == "True" ]]; then
     python3 migrations.py
