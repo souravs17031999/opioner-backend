285120e796fe6f04602525f02c7a4961b189b9c7
souravs17031999 <souravs_1999@rediffmail.com>
2022-03-06 18:25:30 +0530

    wip

--- auth-service/docker-entrypoint.sh
+++ auth-service/docker-entrypoint.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 export FLASK_APP=app.py
-export FLASK_ENV=development 
+export FLASK_ENV=development
 export PYTHONUNBUFFERED="true"
-
 source env.sh
-
-source wait-for-db-server.sh 
-
-if [[ "$REQUIRE_DB_MIGRATIONS" == "True" ]]; then 
+source wait-for-db-server.sh
+if [[ "$REQUIRE_DB_MIGRATIONS" == "True" ]]; then
     python3 migrations.py
 fi
-
 python3 -m flask run --host=0.0.0.0 --port=$PORT
