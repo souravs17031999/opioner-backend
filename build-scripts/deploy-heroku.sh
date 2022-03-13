7f04b09e0c769d385a37fe34ff0e555e4f25a1a3
souravs17031999 <souravs_1999@rediffmail.com>
2022-03-13 17:14:27 +0530

    Update Heroku scripts to deploy to CI/CD

--- build-scripts/deploy-heroku.sh
+++ build-scripts/deploy-heroku.sh
@@ -12,36 +12,36 @@ echo $HEROKU_APPS
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-if [[ "auth" =~ .*"$HEROKU_APPS".* ]]; then
-  echo "deploying auth service to production ...."
+if [[ "$HEROKU_APPS" =~ .*"auth-service-prd".* ]]; then
+  echo "deploying opioner auth service to production ...."
   cd $TOPDIR/auth-service 
   heroku container:push web -a auth-service-prd
   heroku container:release web -a auth-service-prd
 fi 
 
-if [[ "product" =~ .*"$HEROKU_APPS".* ]]; then
-  echo "deploying product service to production ...."
+if [[ "$HEROKU_APPS" =~ .*"product-service-prd".* ]]; then
+  echo "deploying opioner product service to production ...."
   cd $TOPDIR/product-service 
   heroku container:push web -a product-service-prd
   heroku container:release web -a product-service-prd
 fi 
 
-if [[ "user" =~ .*"$HEROKU_APPS".* ]]; then
-  echo "deploying user service to production ...."
+if [[ "$HEROKU_APPS" =~ .*"user-service-prd01".* ]]; then
+  echo "deploying opioner user service to production ...."
   cd $TOPDIR/user-service 
   heroku container:push web -a user-service-prd01
   heroku container:release web -a user-service-prd01
 fi 
 
-if [[ "notification" =~ .*"$HEROKU_APPS".* ]]; then
-  echo "deploying notification service to production ...."
+if [[ "$HEROKU_APPS" =~ .*"notification-service-prd".* ]]; then
+  echo "deploying opioner notification service to production ...."
   cd $TOPDIR/notification-service 
   heroku container:push web -a notification-service-prd
   heroku container:release web -a notification-service-prd
 fi 
 
-if [[ "cron" =~ .*"$HEROKU_APPS".* ]]; then
-  echo "deploying cron service to production ...."
+if [[ "$HEROKU_APPS" =~ .*"cron-worker-prd".* ]]; then
+  echo "deploying opioner cron service to production ...."
   cd $TOPDIR/cron-service
   heroku container:push web -a cron-worker-prd
   heroku container:release web -a cron-worker-prd
