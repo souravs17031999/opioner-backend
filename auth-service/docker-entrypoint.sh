b958c14877a1a49881be5cc3826065592d2a3488
souravs17031999 <souravs_1999@rediffmail.com>
2022-03-19 14:13:09 +0530

    Add New Relic monitoring tool

--- auth-service/docker-entrypoint.sh
+++ auth-service/docker-entrypoint.sh
@@ -7,4 +7,9 @@ source wait-for-db-server.sh
 if [[ "$REQUIRE_DB_MIGRATIONS" == "True" ]]; then
     python3 migrations.py
 fi
-python3 -m flask run --host=0.0.0.0 --port=$PORT
+if [[ "$APM_MONITORING_NEWRELIC" == "True" ]]; then
+    echo "Starting with APM monitoring (NewRelic)"
+    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 -m flask run --host=0.0.0.0 --port=$PORT
+else
+    python3 -m flask run --host=0.0.0.0 --port=$PORT
+fi
