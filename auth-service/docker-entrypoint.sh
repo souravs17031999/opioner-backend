15fd5215863f6b5df60e6136e7d0da9540aefdf3
SOUKUM <sourav.kumar@barco.com>
2022-11-06 17:54:06 +0530

    Remove NewRelic integration

--- auth-service/docker-entrypoint.sh
+++ auth-service/docker-entrypoint.sh
@@ -7,9 +7,5 @@ source wait-for-db-server.sh
 if [[ "$REQUIRE_DB_MIGRATIONS" == "True" ]]; then
     python3 migrations.py
 fi
-if [[ "$APM_MONITORING_NEWRELIC" == "True" ]]; then
-    echo "Starting with APM monitoring (NewRelic)"
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 -m flask run --host=0.0.0.0 --port=$PORT
-else
-    python3 -m flask run --host=0.0.0.0 --port=$PORT
-fi
+
+python3 -m flask run --host=0.0.0.0 --port=$PORT
--- notification-service/docker-entrypoint.sh
+++ notification-service/docker-entrypoint.sh
@@ -4,9 +4,5 @@ export FLASK_ENV=development
 export PYTHONUNBUFFERED="true"
 source env.sh
 source wait-for-db-server.sh
-if [[ "$APM_MONITORING_NEWRELIC" == "True" ]]; then
-    echo "Starting with APM monitoring (NewRelic)"
-    NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 -m flask run --host=0.0.0.0 --port=$PORT
-else
-    python3 -m flask run --host=0.0.0.0 --port=$PORT
-fi
+
+python3 -m flask run --host=0.0.0.0 --port=$PORT
